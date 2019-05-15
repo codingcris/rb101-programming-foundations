272d3420b07d49d441373cfdb29b6ed647c7195d
Cristian Reyes <christianreyes.ail@gmail.com>
2019-05-15 14:45:00 -0600

    Modified the loan calculator application to now have seperate methods for retrieving loan_ammount, apr, and years variables from the user and updated the valid_num? method to verify user input more accurately. Overall the application is more modular because of these changes.

--- lesson_2/loan-calculator/loan_calculator.rb
+++ lesson_2/loan-calculator/loan_calculator.rb
@@ -1,3 +1,4 @@
+require 'bigdecimal'
 require 'yaml'
 MESSAGES = YAML.load_file('loan_calculator_messages.yml')
 
@@ -5,11 +6,73 @@ def prompt(msg)
   puts("=> #{msg}")
 end
 
+def retrieve_loan_amount
+  loop do
+    prompt(MESSAGES['loan_amount'])
+    print('$')
+    amount = gets.chomp
+    # remove commas if user inputs a number in the format 1,000
+    amount = amount.split(",").join if amount.include?(",")
+    return amount if valid_num?(amount)
+    prompt(MESSAGES["invalid"])
+  end
+end
+
+def retrieve_apr
+  loop do
+    prompt(MESSAGES['apr'])
+    apr = gets.chomp
+    return apr if valid_num?(apr)
+    prompt(MESSAGES['invalid'])
+  end
+end
+
+def retrieve_duration
+  loop do
+    prompt(MESSAGES['loan_duration'])
+    years = gets.chomp
+    return years if valid_num?(years)
+    prompt(MESSAGES['invalid'])
+  end
+end
+
 def valid_num?(number)
-  return false if number.empty? || number.to_i < 0
-  if number.to_i.to_s == number
-    true
-  else number.to_f.to_s == number
+  return false if number.empty? || number.to_f <= 0
+  true if number.to_i.to_s == number || number.to_f.to_s == number
+end
+
+def payment_calc(loan_amount, monthly_rate, months)
+  loan_amount.to_f * (monthly_rate / (1 - (1 + monthly_rate)**-months))
+end
+
+def show_summary(loan_amount, apr, months)
+  info = {
+    'loan amount' => '$' + loan_amount.to_s,
+    'APR' => apr.to_s + '%',
+    'loan duration' => months.to_s + ' months'
+  }
+
+  prompt(MESSAGES['summary'])
+  puts(MESSAGES['divider'])
+  info.each { |key, value| puts(key.ljust(15) + value.to_s) }
+  puts(MESSAGES['divider'])
+end
+
+def show_payment(monthly_payment, months, monthly_rate)
+  prompt(format("Your monthly payment is $%.2<payment>f for #{months} months \
+with a monthly interest rate of %.4<rate>f%", payment: monthly_payment,
+                                              rate: monthly_rate * 100))
+end
+
+def again?
+  loop do
+    prompt(MESSAGES['again'])
+    response = gets.chomp.downcase
+    if response == 'y'
+      Gem.win_platform? ? (system "cls") : (system "clear")
+    end
+    return response if response == 'y' || response == 'n'
+    prompt(MESSAGES['invalid_again'])
   end
 end
 
@@ -17,52 +80,19 @@ prompt(MESSAGES['welcome'])
 prompt(MESSAGES['uses'])
 
 loop do
-  loan_ammount = ''
-  loop do
-    prompt(MESSAGES['loan_ammount'])
-    print('$')
-    loan_ammount = gets.chomp
-    # remove commas if user inputs a number in the format 1,000
-    loan_ammount = loan_ammount.split(",").join if loan_ammount.include?(",")
-    break if valid_num?(loan_ammount)
-    prompt(MESSAGES["invalid"])
-  end
+  loan_amount = retrieve_loan_amount
+  apr = retrieve_apr
+  years = retrieve_duration
 
-  apr = ''
-  loop do
-    prompt(MESSAGES['apr'])
-    apr = gets.chomp
-    break if valid_num?(apr)
-    prompt(MESSAGES['invalid'])
-  end
+  monthly_rate = (apr.to_f / 100) / 12
+  months = (years.to_f * 12).to_i
+  monthly_payment = payment_calc(loan_amount, monthly_rate, months)
 
-  loan_duration = ''
-  loop do
-    prompt(MESSAGES['loan_duration'])
-    loan_duration = gets.chomp
-    break if valid_num?(loan_duration)
-    prompt(MESSAGES['invalid'])
-  end
+  show_summary(loan_amount, apr, months)
+  show_payment(monthly_payment, months, monthly_rate)
 
-  monthly_interest_rate = (apr.to_f / 100) / 12
-  duration_to_months = (loan_duration.to_f * 12).to_i
-  monthly_payment = loan_ammount.to_f * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**-duration_to_months))
-
-  info = {
-    'loan ammount' => '$' + loan_ammount.to_s,
-    'APR' => apr.to_s + '%',
-    'loan duration' => loan_duration.to_s + ' years'
-  }
-
-  prompt(MESSAGES['summary'])
-  puts(MESSAGES['divider'])
-  info.each { |key, value| puts(key.ljust(15) + value.to_s) }
-  puts(MESSAGES['divider'])
-
-  prompt("Your monthly payment is $%.2f for #{duration_to_months} months with a monthly interest rate of %.4f%" % [monthly_payment, (monthly_interest_rate * 100)])
-  prompt(MESSAGES['again'])
-  response = gets.chomp.downcase
-  if response != 'y'
+  response = again?
+  unless response == 'y'
     prompt(MESSAGES['goodbye'])
     break
   end
